a9cd1fc8d09880c93dd81d12cdc543bd85740215
ChristopherGeidl <christophergeidl@gmail.com>
2025-09-16 17:44:32 -0700

    Add borderless option

--- simulate.py
+++ simulate.py
@@ -6,9 +6,12 @@ from scipy.ndimage import gaussian_filter
 # Grid setup
 GRID_WIDTH, GRID_HEIGHT = 100, 100  #size of grid
 dx = dy = 1.0 # spacial step size between grid points (1 unit apart)
-c = 2.0 # wave speed
+c = 4.0 # wave speed
 dt = 0.1 #times step (amount of time wave advances each frame) CFL condition c*(dt/dx) <= 1/sqrt(2)
 steps = 500 # num frames
+
+def round_half_up(n):
+    return int(n+0.5) if n > 0 else int(n-0.5)
 
 # Wave field arrays
 u = np.zeros((GRID_WIDTH, GRID_HEIGHT))      # current
@@ -33,7 +36,7 @@ def generateWalls():
 # Initial disturbance
 generateDisturbance()
 
-#wall
+#wall mask
 mask = np.zeros_like(u, dtype=bool)
 for w in walls:
     mask[w[0][0] : w[1][0], w[0][1] : w[1][1]] = True
@@ -46,18 +49,27 @@ fig, ax = plt.subplots()
 im = ax.imshow(u_masked, cmap=cmap, vmin=-0.4, vmax=0.4)
 
 disturbTime = 0
+wavelength = 20
+borderless = True
 def update(frame):
     global u, u_new, u_old, disturbTime
 
     #produces waves
-    if disturbTime == 20:
+    if disturbTime == wavelength:
         generateDisturbance()
         disturbTime = 0
     else:
         disturbTime += 1
     
-    # wall?
+    # wall
     generateWalls()
+
+    #changes whether waves bounce off of window borders
+    if borderless:
+        u[0, :] = u[1, :]
+        u[-1, :] = u[-2, :]
+        u[:, 0] = u[:, 1]
+        u[:, -1] = u[:, -2]
 
     # finite difference wave equation
     u_new[1:-1,1:-1] = (2*u[1:-1,1:-1] - u_old[1:-1,1:-1] + (c*dt/dx)**2 * (u[2:,1:-1] + u[:-2,1:-1] + u[1:-1,2:] + u[1:-1,:-2] - 4*u[1:-1,1:-1]))
@@ -66,7 +78,7 @@ def update(frame):
     u_old, u, u_new = u, u_new, u_old
 
     #smooth
-    u_smooth = gaussian_filter(u, sigma=1)
+    u_smooth = gaussian_filter(u, sigma=round_half_up(0.05*wavelength))
     u_masked = np.ma.masked_array(u_smooth, mask=mask)
 
     im.set_array(u_masked)
