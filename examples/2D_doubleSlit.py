25035a12e61505ed3e2f4acbfe6602ec946f4634
ChristopherGeidl <christophergeidl@gmail.com>
2025-09-17 16:49:36 -0700

    Create Wave_2D class and create double slit example

---
+++ examples/2D_doubleSlit.py
@@ -0,0 +1,19 @@
+import sys
+import os
+
+# Add parent directory (Wave-Simulation) to sys.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+import simulate
+
+GRID_WIDTH = GRID_HEIGHT = 100
+dx = dy = 1.0
+c = 4.0
+wavelength = 20
+frames = 50
+disturbancePoints = [(GRID_WIDTH-5, GRID_HEIGHT//2)]
+
+# two points define wall
+walls = [[(60, 0),(61, GRID_WIDTH//2 - 10)], [(60, GRID_WIDTH//2 - 5),(61, GRID_WIDTH//2 + 5)],[(60, GRID_WIDTH//2 + 10),(61, GRID_WIDTH)]]
+
+wave = simulate.Wave_2D(GRID_WIDTH, GRID_HEIGHT, dx, dy, c, wavelength, frames, disturbancePoints, walls)
